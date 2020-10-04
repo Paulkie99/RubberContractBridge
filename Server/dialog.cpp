6c4235ab6e09d0f5f8a90090345fa1de0f71617b
Paul <u18006885@tuks.co.za>
2020-10-04 17:12:29 +0200

    Server-Side Communication Testable

---
+++ Server/dialog.cpp
@@ -0,0 +1,26 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent, quint16 port)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    bridgeServer = new Server("Bridge Server", QWebSocketServer::NonSecureMode, this);
+    bridgeServer->listen(QHostAddress::Any, port);
+    connect(bridgeServer, SIGNAL(messageReceived(QString)),
+            this, SLOT(messageReceived(QString)));
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::messageReceived(const QString &message)
+{
+    ui->listWidget->addItem(message);
+}
+
+
+
